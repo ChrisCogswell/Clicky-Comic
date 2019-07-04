0bb234ba4871a7dea1f471395d9fa521aef1c777
ChrisCogs <ccogs@yahoo.com>
2019-07-04 14:20:52 -0400

    Modified HeroCard and App.js files. Added heroes.json file.

--- clicky-react/src/App.js
+++ clicky-react/src/App.js
@@ -1,30 +1,21 @@
 import React from "react";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import FriendCard from "./components/FriendCard";
-import friends from "./friends.json";
+import HeroCard from "./components/HeroCard";
+import heroes from "./heroes.json";
 
 function App() {
   return (
     <Wrapper>
-      <Title>Friends List</Title>
-      <FriendCard
-        name={friends[0].name}
-        image={friends[0].image}
-        occupation={friends[0].occupation}
-        location={friends[0].location}
+      <Title>Heroes</Title>
+      <HeroCard
+        image={heroes[0].image}
       />
-      <FriendCard
-        name={friends[1].name}
-        image={friends[1].image}
-        occupation={friends[1].occupation}
-        location={friends[1].location}
+      <HeroCard
+        image={heroes[1].image}
       />
-      <FriendCard
-        name={friends[2].name}
-        image={friends[2].image}
-        occupation={friends[2].occupation}
-        location={friends[2].location}
+      <HeroCard
+        image={heroes[2].image}
       />
     </Wrapper>
   );
