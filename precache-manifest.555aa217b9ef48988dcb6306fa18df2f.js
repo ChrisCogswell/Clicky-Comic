f1f87ffa79b91f7f3c2d67d6772747a2cff35973
ChrisCogs <ccogs@yahoo.com>
2019-07-06 22:49:52 -0400

    Updates

---
+++ precache-manifest.555aa217b9ef48988dcb6306fa18df2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d032d2ddaad3fabd512b33a0b2a289c",
+    "url": "/Clicky-Comic/index.html"
+  },
+  {
+    "revision": "4b31a732a16948109bef",
+    "url": "/Clicky-Comic/static/css/main.f605e740.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Comic/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "4b31a732a16948109bef",
+    "url": "/Clicky-Comic/static/js/main.8b0bcfb1.chunk.js"
+  },
+  {
+    "revision": "5ba56a99a34d58ed2c04",
+    "url": "/Clicky-Comic/static/js/runtime~main.5e3cd409.js"
+  }
+]);
